2809f3d4370f431e7a21228983e2d77e95c5734a
Gabriel Dornas <gabrielbdornas@gmail.com>
2024-01-08 14:39:26 -0300

    Wait page load
    See #2

--- scripts/zvg_scraping.py
+++ scripts/zvg_scraping.py
@@ -1,6 +1,10 @@
 import datetime
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
+
 
 def find_today():
   today = datetime.datetime.now()
@@ -17,8 +21,17 @@ def driver_initiate():
     #chrome_options.headless = headless
     driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(3)
-    driver.get('https://www.zvg-portal.de/index.php?button=Termine%20suchen')
+    url = 'https://www.zvg-portal.de/index.php?button=Termine%20suchen'
+    driver.get(url)
+    text_to_check = "Zwangsversteigerungstermine"
+    element = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.TAG_NAME, "body")))
+    if text_to_check in element.text:
+        print("Page loaded.")
+    else:
+        print("Page not loaded.")
     return driver
+
 
 if __name__ == '__main__':
     scraping_process_begin()
